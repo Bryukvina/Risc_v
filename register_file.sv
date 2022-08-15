6ac2e8720cab9c5ee7d43655730bccb84864b5d7
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2022-08-15 16:04:25 +0300

    Update register_file.sv

--- register_file.sv
+++ register_file.sv
@@ -1,26 +1,23 @@
-module (
-    input   clk,
-    input   a1,
-    input   a2,
-    input   a3,
-    input   wd3,
-    input   we3,
+module register_file
+(
+    input         clk,
 
-    output  rd1,
-    output  rd2
+    input  [ 4:0] a1,
+    input  [ 4:0] a2,
+    input  [ 4:0] a3,
+
+    input  [31:0] wd3,
+    input         we3,
+
+    output [31:0] rd1,
+    output [31:0] rd2
+
 );
-
     reg [31:0] rf [31:0];
 
-    assign rd0 = (a1 != 0) ? rf [a1] : 32'b0;
-    assign rd1 = (a2 != 0) ? rf [a2] : 32'b0;
-    assign rd2 = (a3 != 0) ? rf [a3] : 32'b0;
+    assign rd1 = (a1 != 0) ? rf [a1] : 32'b0;
+    assign rd2 = (a2 != 0) ? rf [a2] : 32'b0;
 
-    always @ (posedge clk) begin
-        if(we3) rf [a3] <= wd3;
-    end
-    
+    always @ (posedge clk)
+        if (we3) rf [a3] <= wd3;
 endmodule
-
-
-
